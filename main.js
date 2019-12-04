7e9ae41b12db8fc2b0167a872012df743c15edf8
Emmerson <donnelem@oregonstate.edu>
2019-12-03 16:22:14 -0800

    FINAL

--- main.js
+++ main.js
@@ -64,6 +64,7 @@ var chart = new Chart(ctx, {
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45]
+       
         }]
     },
 
